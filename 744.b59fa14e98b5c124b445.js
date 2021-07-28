d692fa55c1a5b1e2dd006504665cb9be4b006db7
App-SRE Team <sd-app-sre@redhat.com>
2021-07-28 09:23:47 +0000

    Update to da96a35

--- 744.b59fa14e98b5c124b445.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkapplication_services_ui=self.webpackChunkapplication_services_ui||[]).push([[744,453],{85208:()=>{},6702:()=>{},12993:()=>{},1386:()=>{},22751:()=>{},66355:()=>{}}]);
