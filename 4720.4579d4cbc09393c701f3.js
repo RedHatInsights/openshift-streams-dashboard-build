9eb070fddc2606589212072438d5657387e8cdee
App-SRE Team <sd-app-sre@redhat.com>
2022-06-13 13:03:40 +0000

    Promote 24558ac to prod-beta

--- 4720.4579d4cbc09393c701f3.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkapplication_services_ui=self.webpackChunkapplication_services_ui||[]).push([[4720],{44720:(a,e,s)=>{s.r(e),s.d(e,{KafkaOverViewPage:()=>p,default:()=>u});var i=s(85893),c=s(11702);const p=()=>(0,i.jsx)(c.KafkaPageV2,{}),u=p}}]);
