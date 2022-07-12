6f07ded5e54524d52facde079ba86d5077ef8fdd
Red Hat Application Services CI <app-services-ci@users.noreply.github.com>
2022-07-12 10:13:46 +0000

    Update to 023e8a5

---
+++ 9132.365d556e3bc1df2b6d28.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkapplication_services_ui=self.webpackChunkapplication_services_ui||[]).push([[9132],{9132:(a,e,s)=>{s.r(e),s.d(e,{KafkaOverViewPage:()=>p,default:()=>u});var i=s(85893),c=s(13499);const p=()=>(0,i.jsx)(c.KafkaPageV2,{}),u=p}}]);
+//# sourceMappingURL=9132.365d556e3bc1df2b6d28.js.map
