80b04f7a6a55642048c363f6b85cdd3c9752cc1d
Red Hat Application Services CI <app-services-ci@users.noreply.github.com>
2022-08-05 13:11:04 +0000

    Update to 5f7ecab

--- 4809.59bfbe331b7ba01b61db.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkapplication_services_ui=self.webpackChunkapplication_services_ui||[]).push([[4809],{54809:(e,s,a)=>{a.r(s),a.d(s,{ApiDesignerEditorPage:()=>p,default:()=>u});var i=a(85893),r=a(68698),c=a(3241),n=a(52212),d=a(63645),o=a(68199);const p=()=>{const e=(0,n.useConfig)(),s=(0,o.useParams)();return(null==e?void 0:e.serviceDown)?(0,i.jsx)(c.d,{}):(0,i.jsx)(r.IG,{scope:"ads",fallback:(0,i.jsx)(d.AppServicesLoading,{}),module:"./FederatedEditorPage",render:e=>(0,i.jsx)(e,{params:s})})},u=p}}]);
-//# sourceMappingURL=4809.59bfbe331b7ba01b61db.js.map
---
+++ 9132.49860c9beaf48516f98c.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkapplication_services_ui=self.webpackChunkapplication_services_ui||[]).push([[9132],{9132:(a,e,s)=>{s.r(e),s.d(e,{KafkaOverViewPage:()=>p,default:()=>u});var i=s(85893),c=s(31571);const p=()=>(0,i.jsx)(c.KafkaPageV2,{}),u=p}}]);
+//# sourceMappingURL=9132.49860c9beaf48516f98c.js.map
