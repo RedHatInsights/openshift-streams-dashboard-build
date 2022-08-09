adcc1cf19d91eeb90e453211515d978be12a437f
Red Hat Application Services CI <app-services-ci@users.noreply.github.com>
2022-08-09 14:08:18 +0000

    Update to c610c8e

--- 4880.fae7baa8c3eb2af58f0c.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkapplication_services_ui=self.webpackChunkapplication_services_ui||[]).push([[4880],{54880:(e,s,i)=>{i.r(s),i.d(s,{SettingsPage:()=>c,default:()=>l});var r=i(85893),t=i(868),n=i(51125),a=i(52212),u=i(15675);const c=()=>{const e=(0,a.useConfig)();return(null==e?void 0:e.serviceDown)?(0,r.jsx)(u.dB,{}):(0,r.jsx)(d,{})},d=()=>(0,r.jsx)(n.q,{breadcrumbId:"srs.settings",render:e=>(0,r.jsx)(t.P,{registry:e,module:"./FederatedSettingsPage"})}),l=c}}]);
-//# sourceMappingURL=4880.fae7baa8c3eb2af58f0c.js.map
