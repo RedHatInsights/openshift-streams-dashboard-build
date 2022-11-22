b103a5a0dd66aceaf6558a1db64cab30794b4b14
Red Hat Application Services CI <app-services-ci@users.noreply.github.com>
2022-11-22 07:07:52 +0000

    Update to 586587b

--- 6264.5c87b44db9c3733c7c58.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkapplication_services_ui=self.webpackChunkapplication_services_ui||[]).push([[6264],{56264:(e,s,a)=>{a.r(s),a.d(s,{ApiDesignerHomePage:()=>u,default:()=>d});var i=a(85893),r=a(68698),c=a(3241),n=a(50834),o=a(82774),p=a(73685);const u=()=>{const e=(0,n.useConfig)(),s=(0,p.useParams)();return(null==e?void 0:e.serviceDown)?(0,i.jsx)(c.d,{}):(0,i.jsx)(r.IG,{scope:"ads",fallback:(0,i.jsx)(o.AppServicesLoading,{}),module:"./FederatedHomePage",render:e=>(0,i.jsx)(e,{params:s})})},d=u}}]);
-//# sourceMappingURL=6264.5c87b44db9c3733c7c58.js.map
